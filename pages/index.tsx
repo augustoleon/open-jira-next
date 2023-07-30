997505e1575c203e531c3b067c72a2a2422a0b33
Cesar Leon <cessaleon@gmail.com>
2023-07-30 15:48:42 -0300

    creamos el diseño de la pantalla principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import { Typography } from '@mui/material';
+import { CardHeader, Grid, Card } from '@mui/material';
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    <Layout title='Home - OpenJita'>
+      <Grid container spacing={2}>
+      <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc( 100vh - 100px )' }}>
+            <CardHeader title="Pendientes" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc( 100vh - 100px )' }}>
+            <CardHeader title="En Progenso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc( 100vh - 100px)' }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
 
 export default HomePage;
+
+// MUI trabaja first-mobile, lo que definamos en el 
+// breakpoint 'xs' lo heredaran todos los tipos de pantalla
+// al menos que definamos lo contrario con otro tipo de breakpoint.
+// definimos los breakpoints en el componente xs sm 
+
+// sx, extended style, no confundir con el breakpoint 'xs'
